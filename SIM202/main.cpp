6a545e04e6bcc655568310a25e3d2dd169d8fca9
Abdoul-Hakim Ahamada <124039932+Hakim506@users.noreply.github.com>
2024-04-06 13:26:04 +0200

    Add files via upload

---
+++ SIM202/main.cpp
@@ -0,0 +1,20 @@
+#include "tictactoe.h"
+#include "echec.h"
+
+#include <iostream>
+#include <vector>
+#include <stdlib.h>
+#include <array>
+#include <ctime>
+#include <cstdlib>
+
+using namespace std;
+
+int main()
+{
+//    Grille grille;
+//    Jouer(grille);
+    JouerUnJoueur();
+    JouerDeuxJoueurs();
+    return 0;
+}
